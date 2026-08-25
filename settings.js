78840d1c33c5cfa44869490243a6f924e53c0ce1
Tunahan Kayahan <tunahankayahan24@gmail.com>
2026-08-25 18:07:42 +0300

    Load secrets from environment

--- settings.js
+++ settings.js
@@ -19,7 +19,9 @@
  *  - Node Settings
  *
  **/
-
+require("dotenv").config({
+  path: require("path").join(__dirname, ".env"),
+});
 
 module.exports = {
   /*******************************************************************************
